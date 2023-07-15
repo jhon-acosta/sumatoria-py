90a800212158308c8f4c9754612f605b7005b496
Jhon Acosta <jhonerick2018@gmail.com>
2023-07-15 13:38:48 -0500

    feat(prueba): Solución de problema

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import re
 import socket
 
-SERVER_ADDRESS = '192.168.1.3'
+SERVER_ADDRESS = 'localhost'
 SERVER_PORT = 9090
 
 s = socket.socket()
@@ -9,27 +9,27 @@ s.connect((SERVER_ADDRESS, SERVER_PORT))
 print('Servidor levantado:' + str((SERVER_ADDRESS, SERVER_PORT)))
 
 while True:
-  try:
-    valor1 = input("[CLIENTE]: Ingrese un primer valor numerico:")
-    valor2 = input("[CLIENTE]: Ingrese un segundo valor numerico:")
-  except EOFError:
-      print("Error: inconveniente en la captura de valores")
-  
-  digitos = [valor1, valor2]
+    try:
+        num1 = input("[CLIENTE]: Ingrese un primer valor numerico:")
+        num2 = input("[CLIENTE]: Ingrese un segundo valor numerico:")
+    except EOFError:
+        print("Error: inconveniente en la captura de valores")
 
-  # Validaciones
-  if any(not re.match(r'^\d+(\.\d+)?$' or not valor , valor) for valor in digitos):
-    print("[CLIENTE]: Algun valor ingresado no es numerico")
-    continue
+    digitos = [num1, num2]
 
-  data = ','.join(digitos)
-  s.send(data.encode())
+    # Validaciones
+    if any(not re.match(r'^\d+(\.\d+)?$' or not valor, valor) for valor in digitos):
+        print("[CLIENTE]: Algun valor ingresado no es numerico")
+        continue
 
-  data = s.recv(2048)
-  if not data:
-      print("[CLIENTE]: Servidor habilitado")
-      break
-  else:
-      print(data)
+    data = ','.join(digitos)
+    s.send(data.encode())
+
+    data = s.recv(2048)
+    if not data:
+        print("[CLIENTE]: Servidor habilitado")
+        break
+    else:
+        print(data)
 
 s.close()
--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket
 from functools import reduce
 
-SERVER_ADDRESS = '192.168.1.3'
+SERVER_ADDRESS = 'localhost'
 SERVER_PORT = 9090
 
 s = socket.socket()
@@ -10,22 +10,37 @@ s.listen()
 
 print("Escuchando en el servidor: " + str((SERVER_ADDRESS, SERVER_PORT)))
 
+
+def onVeces(number):
+    array = []
+    for i in range(1, round(number) + 1):
+        array.append(i)
+    return array
+
+
 while True:
-  c, addr = s.accept()
-  while True:
-    data = c.recv(2048)
+    c, addr = s.accept()
+    while True:
+        data = c.recv(2048)
 
-    if not data:
-      print("[SERVIDOR]: Fin de transmisión desde el cliente")
-      break
+        if not data:
+            print("[SERVIDOR]: Fin de transmisión desde el cliente")
+            break
 
-    data = data.decode()
-    data = [format(float(num), '.2f') for num in data.split(',')]
-    data = list(map(float, data))
-    data = reduce(lambda acc, val: acc + val, data, 0)
+        data = data.decode()
+        data = [format(float(num), '.2f') for num in data.split(',')]
+        data = list(map(float, data))
+        data = reduce(lambda acc, val: acc + val, data, 0)
+        print(f'[SERVIDOR]: Sumatoria de ambos números es {data}')
 
-    print(f'[SERVIDOR]: Sumatoria {data}')
+        nArr = onVeces(data)
+        print(f'[SERVIDOR]: Array de n veces de sumatoria es', nArr)
 
-    c.send(f'[SERVIDOR]: La sumatoria es: {data} //'.encode())
+        nArrTotal = reduce(lambda acc, val: acc + val, nArr, 0)
+        response = '[SERVIDOR]: Sumatoria mas n veces es: ' + \
+            str(data + nArrTotal)
+        print(response)
 
-  c.close() 
+        c.send(response.encode())
+
+    c.close()
